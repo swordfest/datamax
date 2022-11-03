85540e15ebb1eff36cf4fec2246a4c0d66ef3a37
Alex Navarro <alexanderhavarro@gmail.com>
2022-11-02 17:28:04 -0700

    changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../index.css';
+// import '../index.css';
 import Navbar from './Navbar';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Captcha from './Captcha';
@@ -8,7 +8,7 @@ import Captcha from './Captcha';
 function Dashboard () {
     return (
         <main className='container w-full h-screen ml-[100px] p-6 bg-slate-400'>
-            <Captcha/>
+            {/* <Captcha/> */}
         </main>
     )
 }
